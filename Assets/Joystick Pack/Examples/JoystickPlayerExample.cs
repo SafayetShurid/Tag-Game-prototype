f056c28b6b9f673e51e317ce1253590ebf93bfa0
Safayet Anowar Shurid <Shurid16@gmail.com>
2021-02-15 11:17:14 +0600

    Initial commit

---
+++ Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public FixedJoystick fixedJoystick;
+    public Rigidbody2D rb;
+
+    public float horizontalMove = 0f;
+    public float verticalMove = 0f;
+
+    public void FixedUpdate()
+    {
+        float x = fixedJoystick.Horizontal;
+        float y = fixedJoystick.Vertical;
+        rb.velocity = new Vector2(x * speed, y * speed);
+      
+    }
+}
---
+++ Assets/TouchControl2.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchControl2 : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject circle, dot;
+    private Rigidbody2D rb;
+    private float moveSpeed;
+    private Touch oneTouch;
+    private Vector2 touchPosition;
+    private Vector2 moveDirection;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        circle.SetActive(false);
+        dot.SetActive(false);
+        moveSpeed = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            oneTouch = Input.GetTouch(0);
+            touchPosition = Camera.main.ScreenToWorldPoint(oneTouch.position);
+            switch (oneTouch.phase)
+            {
+                case TouchPhase.Began:
+                    circle.SetActive(true);
+                    dot.SetActive(true);
+
+                    circle.transform.position = touchPosition;
+                    dot.transform.position = touchPosition;
+                    break;
+                case TouchPhase.Stationary:
+                    MovePlayer();
+                    break;
+                case TouchPhase.Moved:
+                    MovePlayer();
+                    break;
+                case TouchPhase.Ended:
+                    circle.SetActive(false);
+                    dot.SetActive(false);
+
+                    rb.velocity = Vector2.zero;
+                    break;
+            }
+        }
+    }
+    private void MovePlayer()
+    {
+        dot.transform.position = touchPosition;
+
+        dot.transform.position = new Vector2(
+            Mathf.Clamp(dot.transform.position.x,
+            circle.transform.position.x - 0.48f,
+            circle.transform.position.x + 0.48f),
+            Mathf.Clamp(dot.transform.position.x,
+            circle.transform.position.y - 0.48f,
+            circle.transform.position.y + 0.48f));
+
+        moveDirection = (dot.transform.position - circle.transform.position).normalized;
+        rb.velocity = moveDirection * moveSpeed;
+    }
+}
